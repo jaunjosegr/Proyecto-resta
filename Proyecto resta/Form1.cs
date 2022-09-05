acd41872edc57da37da2d5e41acacadb8e9cfbed
jg602 <jg602@JUANJOS>
2022-09-05 08:44:55 -0700

    Agregar archivos de proyecto.

---
+++ Proyecto resta/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_resta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clresta SS = new clresta();
+
+            SS.N1 = int.Parse(textBox1.Text);
+            SS.N2 = int.Parse(textBox2.Text);
+
+            textBox3.Text = SS.resta().ToString();
+
+            clresta KK = new clresta(int.Parse(textBox1.Text),
+            int.Parse(textBox2.Text)); textBox3.Text = KK.resta().ToString();
+
+        }
+    }
+}
---
+++ Proyecto resta/clresta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_resta
+{
+    internal class clresta
+    {
+        private int n1, n2;
+
+        public int N1 { get => n1; set => n1 = value; }
+        public int N2 { get => n2; set => n2 = value; }
+
+        public clresta(int n1, int n2)
+        {
+            this.N1 = n1;
+            this.N2 = n2;
+        }
+
+        public int resta()
+        {
+            return N1 - N2;
+        }
+
+        public clresta() 
+        {
+
+        }
+    }   
+
+}
